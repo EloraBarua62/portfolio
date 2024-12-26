b7b74f1ceab15b3839528814b5348f46234817d2
EloraBarua <elorabarua62@gmail.com>
2024-12-26 19:06:30 +0200

    side responsive done

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -4,6 +4,7 @@ import styles from "./contact.module.scss";
 import { IoPersonSharp } from "react-icons/io5";
 import { MdOutlineAlternateEmail } from "react-icons/md";
 import { MdOutlineMailOutline } from "react-icons/md";
+import Link from "next/link";
 
 const Contact = () => {
   const [selectField, setSelectField] = useState("");
@@ -38,7 +39,19 @@ const Contact = () => {
             </div>
             <div style={{ display: "flex", justifyContent: "space-between" }}>
               <p className={styles.title}>{info.property_title2}</p>
-              <p className={styles.value}>{info.property_value2}</p>
+              {info.property_title2 == "LinkedIn : " ? (
+                <Link
+                  className={styles.value}
+                  href="https://www.linkedin.com/in/elora-barua-684157206/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  style={{ color: 'white'}}
+                >
+                  {info.property_value2}
+                </Link>
+              ) : (
+                <p className={styles.value}>{info.property_value2}</p>
+              )}
             </div>
             <div style={{ display: "flex", justifyContent: "space-between" }}>
               <p className={styles.title}>{info.property_title3}</p>
@@ -63,7 +76,7 @@ const Contact = () => {
           >
             <IoPersonSharp />
           </div>
-            <input type="text" onSelect={() => setSelectField("person")} />
+          <input type="text" onSelect={() => setSelectField("person")} />
         </div>
         <div className={styles.field_design1}>
           <div
@@ -73,7 +86,7 @@ const Contact = () => {
           >
             <MdOutlineAlternateEmail />
           </div>
-            <input type="text" onSelect={() => setSelectField("email")} />
+          <input type="text" onSelect={() => setSelectField("email")} />
         </div>
         <div className={styles.field_design2}>
           <div
@@ -83,7 +96,7 @@ const Contact = () => {
           >
             <MdOutlineMailOutline />
           </div>
-            <textarea type="text" onSelect={() => setSelectField("message")} />
+          <textarea type="text" onSelect={() => setSelectField("message")} />
         </div>
       </form>
     </div>
